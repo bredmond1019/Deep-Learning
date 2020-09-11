f4999dbdc804aa6ee0c0367707e9b4c9a53acef6
Brandon <bredmond1019@gmail.com>
2020-09-11 10:22:35 -0400

    Updated Repository

---
+++ deepLearning.py
@@ -0,0 +1,259 @@
+'''
+Practice Problem 1
+'''
+
+# c1 = 3
+# c2 = 4
+# b = -10
+
+# x1 = 1
+# x2 = 1
+# bias = 1
+# l = 0.1
+# count = 0
+
+# value = c1 * x1 + c2 * x2 + b
+
+# while (c1 * x1 + c2 * x2 + b < 0):
+#     print(f"c1: {c1}, c2: {c2}, b: {b}, count: {count} and value: {c1 * x1 + c2 * x2 + b}")
+    
+#     c1 = c1 + x1 * l
+#     c2 = c2 + x2 * l
+#     b = b + bias * l
+#     count += 1
+
+# print(c1, c2, count, c1 * x1 + c2 * x2 + b)
+
+# Their Solution
+def perceptronStep(X, y, W, b, learn_rate = 0.01):
+    for i in range(len(X)):
+        y_hat = prediction(X[i],W,b)
+        if y[i]-y_hat == 1:
+            W[0] += X[i][0]*learn_rate
+            W[1] += X[i][1]*learn_rate
+            b += learn_rate
+        elif y[i]-y_hat == -1:
+            W[0] -= X[i][0]*learn_rate
+            W[1] -= X[i][1]*learn_rate
+            b -= learn_rate
+    return W, b
+
+
+# Discrete v Continuous -- Sigmoid Function
+from math import exp
+
+points = [(1,1), (2,4), (5,-5), (-4,5)]
+
+def score(point):
+    result = 4 * point[0] + 5 * point[1] - 9
+    return result
+
+def sigmoid(x):
+    return 1 / (1 + exp(-x))
+
+for point in points:
+    print(sigmoid(score(point)))
+
+
+
+import numpy as np
+
+def softmax(L):
+    expL = np.exp(L)
+    sumExpL = sum(expL)
+    result = []
+    for i in expL:
+        result.append(i*1.0/sumExpL)
+    return result
+    
+    # Note: The function np.divide can also be used here, as follows:
+    # def softmax(L):
+    #     expL = np.exp(L)
+    #     return np.divide (expL, expL.sum())
+
+
+
+
+# Cross Entropy
+import numpy as np
+
+def cross_entropy(Y, P):
+    Y = np.float_(Y)
+    P = np.float_(P)
+    return -np.sum(Y * np.log(P) + (1 - Y) * np.log(1 - P))
+
+
+
+
+
+
+
+'''
+Gradient Descent Practice
+'''
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+#Some helper functions for plotting and drawing lines
+
+def plot_points(X, y):
+    admitted = X[np.argwhere(y==1)]
+    rejected = X[np.argwhere(y==0)]
+    plt.scatter([s[0][0] for s in rejected], [s[0][1] for s in rejected], s = 25, color = 'blue', edgecolor = 'k')
+    plt.scatter([s[0][0] for s in admitted], [s[0][1] for s in admitted], s = 25, color = 'red', edgecolor = 'k')
+
+def display(m, b, color='g--'):
+    plt.xlim(-0.05,1.05)
+    plt.ylim(-0.05,1.05)
+    x = np.arange(-10, 10, 0.1)
+    plt.plot(x, m*x+b, color)
+
+
+
+# Reading the data file and plotting the points
+
+data = pd.read_csv('data.csv', header=None)
+X = np.array(data[[0,1]])
+y = np.array(data[2])
+plot_points(X,y)
+plt.show()
+
+
+
+
+
+
+# Implement the following functions
+
+# Activation (sigmoid) function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# Output (prediction) formula
+def output_formula(features, weights, bias):
+    return sigmoid(np.dot(features, weights) + bias)
+
+# Error (log-loss) formula
+def error_formula(y, output):
+    return -y * np.log(output) - (1 - y) * np.log(1 - output)
+
+# Gradient descent step
+def update_weights(x, y, weights, bias, learnrate):
+    output = output_formula(x, weights, bias)
+    weights = weights + learnrate * (y - output) * x
+    bias = bias + learnrate * (y - output)
+    return weights, bias
+
+
+
+
+
+
+
+
+# Here is the training function
+
+
+np.random.seed(44)
+
+epochs = 100
+learnrate = 0.01
+
+def train(features, targets, epochs, learnrate, graph_lines=False):
+    
+    errors = []
+    n_records, n_features = features.shape
+    last_loss = None
+    weights = np.random.normal(scale=1 / n_features**.5, size=n_features)
+    bias = 0
+    for e in range(epochs):
+        del_w = np.zeros(weights.shape)
+        for x, y in zip(features, targets):
+            output = output_formula(x, weights, bias)
+            error = error_formula(y, output)
+            weights, bias = update_weights(x, y, weights, bias, learnrate)
+        
+        # Printing out the log-loss error on the training set
+        out = output_formula(features, weights, bias)
+        loss = np.mean(error_formula(targets, out))
+        errors.append(loss)
+        if e % (epochs / 10) == 0:
+            print("\n========== Epoch", e,"==========")
+            if last_loss and last_loss < loss:
+                print("Train loss: ", loss, "  WARNING - Loss Increasing")
+            else:
+                print("Train loss: ", loss)
+            last_loss = loss
+            predictions = out > 0.5
+            accuracy = np.mean(predictions == targets)
+            print("Accuracy: ", accuracy)
+        if graph_lines and e % (epochs / 100) == 0:
+            display(-weights[0]/weights[1], -bias/weights[1])
+            
+
+    # Plotting the solution boundary
+    plt.title("Solution boundary")
+    display(-weights[0]/weights[1], -bias/weights[1], 'black')
+
+    # Plotting the data
+    plot_points(features, targets)
+    plt.show()
+
+    # Plotting the error
+    plt.title("Error Plot")
+    plt.xlabel('Number of epochs')
+    plt.ylabel('Error')
+    plt.plot(errors)
+    plt.show()
+
+
+
+
+
+
+# GRADIENT DESCENT CODE
+
+
+
+# Defining the sigmoid function for activations
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+# Derivative of the sigmoid function
+def sigmoid_prime(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+# Input data
+x = np.array([0.1, 0.3])
+# Target
+y = 0.2
+# Input to output weights
+weights = np.array([-0.8, 0.5])
+
+# The learning rate, eta in the weight step equation
+learnrate = 0.5
+
+# the linear combination performed by the node (h in f(h) and f'(h))
+h = x[0]*weights[0] + x[1]*weights[1]
+# or h = np.dot(x, weights)
+
+# The neural network output (y-hat)
+nn_output = sigmoid(h)
+
+# output error (y - y-hat)
+error = y - nn_output
+
+# output gradient (f'(h))
+output_grad = sigmoid_prime(h)
+
+# error term (lowercase delta)
+error_term = error * output_grad
+
+# Gradient descent step 
+del_w = [ learnrate * error_term * x[0],
+          learnrate * error_term * x[1]]
+# or del_w = learnrate * error_term * x
